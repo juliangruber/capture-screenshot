40bb33ae988a1f84c98a1c7f53d8f90ef66fa4fc
Julian Gruber <julian@juliangruber.com>
2013-08-26 18:49:31 +0200

    use local server for tests

--- test.js
+++ test.js
@@ -1,49 +1,58 @@
 var test = require('tape');
 var Screenshot = require('./');
+var http = require('http');
 
-test('screenshot', function(t) {
-  t.plan(3);
-  Screenshot('http://google.com')
-    .capture(function(err, pic) {
-      t.error(err);
-      t.ok(pic);
-      t.ok(Buffer.isBuffer(pic));
-    });
-});
+var server = http.createServer(function(req, res) {
+  res.end('ohai!');
+}).listen(function() {
+  var url = 'http://localhost:' + server.address().port;
 
-test('custom args', function(t) {
-  var s = Screenshot('http://google.com')
-    .width(1024)
-    .height(768)
-    .timeout(100)
-    .format('jpeg');
-  t.equal(s._width, 1024);
-  t.equal(s._height, 768);
-  t.equal(s._timeout, 100);
-  t.equal(s._format, 'JPG');
-  t.end();
-});
-
-test('obj', function(t) {
-  var s = Screenshot('http://google.com', {
-    width: 1024,
-    height: 768,
-    timeout: 100,
-    format: 'jpeg'  
+  test('screenshot', function(t) {
+    t.plan(3);
+    Screenshot(url)
+      .capture(function(err, pic) {
+        t.error(err);
+        t.ok(pic);
+        t.ok(Buffer.isBuffer(pic));
+      });
   });
-  t.equal(s._width, 1024);
-  t.equal(s._height, 768);
-  t.equal(s._timeout, 100);
-  t.equal(s._format, 'JPG');
-  t.end();
+
+  test('custom args', function(t) {
+    var s = Screenshot(url)
+      .width(1024)
+      .height(768)
+      .timeout(100)
+      .format('jpeg');
+    t.equal(s._width, 1024);
+    t.equal(s._height, 768);
+    t.equal(s._timeout, 100);
+    t.equal(s._format, 'JPG');
+    t.end();
+  });
+
+  test('obj', function(t) {
+    var s = Screenshot(url, {
+      width: 1024,
+      height: 768,
+      timeout: 100,
+      format: 'jpeg'  
+    });
+    t.equal(s._width, 1024);
+    t.equal(s._height, 768);
+    t.equal(s._timeout, 100);
+    t.equal(s._format, 'JPG');
+    t.end();
+  });
+
+  test('generator', function(t) {
+    t.plan(3);
+    Screenshot(url)
+      .capture()(function(err, pic) {
+        t.error(err);
+        t.ok(pic);
+        t.ok(Buffer.isBuffer(pic));
+        server.close();
+      });
+  });
 });
 
-test('generator', function(t) {
-  t.plan(3);
-  Screenshot('http://google.com')
-    .capture()(function(err, pic) {
-      t.error(err);
-      t.ok(pic);
-      t.ok(Buffer.isBuffer(pic));
-    });
-});
